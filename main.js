f38a0d3fe64dddfe62eaf175333dc45ec58cb942
namln2k <namln2aug2k@gmail.com>
2021-09-09 20:30:20 +0700

    update 09-09-21

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 function checkLoginState() {
     FB.getLoginStatus(function (response) {
         if (response.status == "connected") {
-            console.log(response);
+            var userID = response.authResponse.userID;
+            //"http://graph.facebook.com/" + userID + "/picture?type=square";
+            console.log(userID);
         }
         else alert("Đăng nhập thất bại!");
     });
