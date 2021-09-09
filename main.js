ccb8d68fcff889cad2d7c10b67b5ac9e65f1555a
namln2k <namln2aug2k@gmail.com>
2021-09-09 22:53:47 +0700

    test

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ function checkLoginState() {
             var userID = response.authResponse.userID;
             //"http://graph.facebook.com/" + userID + "/picture?type=square";
             console.log(response);
+
+            FB.api('/me', { fields: 'id' }, function (response) {
+                console.log(response);
+            });
         }
         else alert("Đăng nhập thất bại!");
     });
