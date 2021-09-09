72818bfa15f647fcc98bd690f28418337f415157
namln2k <namln2aug2k@gmail.com>
2021-09-10 00:16:41 +0700

    test

--- main.js
+++ main.js
@@ -1,29 +1,5 @@
 function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-        if (response.status == "connected") {
-            FB.api('/me', { fields: 'id' }, function (response) {
-                FB.api('/' + response.id + '/picture?redirect=false', function (response) {
-                    userAvatarBeforeUrl = response.data.url;
-                    var canvas = document.createElement("canvas");
-                    document.body.appendChild(canvas);
-                    var ctx = canvas.getContext("2d");
-                    var imageObj1 = new Image(100, 100);
-                    var imageObj2 = new Image(100, 100);
-                    imageObj1.src = userAvatarBeforeUrl;
-                    imageObj2.src = "./frame.png";
-                    imageObj1.onload = function () {
-                        ctx.drawImage(imageObj1, 0, 0, 100, 100);
-                        ctx.drawImage(imageObj2, 0, 0, 100, 100);
-                    };
-                    var downloadButton = document.createElement("button");
-                    downloadButton.value = "Download";
-                    document.body.appendChild(downloadButton);
-                    downloadButton.onclick = function () {
-                        console.log("downloading");
-                    }
-                });
-            });
-        }
-        else alert("Đăng nhập thất bại!");
+    document.getElementById("content").croppie({
+        url: 'frame.png',
     });
 }
