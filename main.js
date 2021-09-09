a008b286588fd06d61555c0235ac8da8a5edd23f
namln2k <namln2aug2k@gmail.com>
2021-09-10 00:21:52 +0700

    test

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-function checkLoginState() {
-    document.getElementById("content").croppie({
-        url: 'frame.png',
-    });
-}
+document.getElementById("content").croppie({
+    url: 'frame.png',
+});
