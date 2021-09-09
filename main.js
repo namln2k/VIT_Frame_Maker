472e2e9781127d11092f79049584f28b06aae786
namln2k <namln2aug2k@gmail.com>
2021-09-09 23:50:48 +0700

    test

--- main.js
+++ main.js
@@ -1,27 +1,18 @@
-FB.getLoginStatus(function (response) {
-    if (response.status == "connected")
-        FB.logout(function (response) {
-            console.log(response);
-        });
-})
-
 function checkLoginState() {
     FB.getLoginStatus(function (response) {
         if (response.status == "connected") {
-            var userID = response.authResponse.userID;
-            //"http://graph.facebook.com/" + userID + "/picture?type=square";
-            console.log(response);
-
             FB.api('/me', { fields: 'id' }, function (response) {
                 console.log(response);
                 FB.api('/' + response.id + '/picture?redirect=false', function (response) {
-                    console.log(response);
+                    console.log(response.data);
                     userAvatarBeforeUrl = response.data.url;
                     var canvas = document.createElement("canvas");
                     document.body.appendChild(canvas);
                     var ctx = canvas.getContext("2d");
                     var imageObj1 = new Image();
                     var imageObj2 = new Image("./frame.png");
+                    console.log("Frame:\nwidth: " + imageObj2.width + "\height: " +imageObj2.height);
+                    console.log("Avatar:\nwidth: " + imageObj1.width + "\height: " +imageObj1.height);
                     imageObj1.src = userAvatarBeforeUrl;
                     imageObj1.onload = function () {
                         ctx.drawImage(imageObj1, 0, 0, 100, 100);
