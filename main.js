4334a50fbafa7eb321beb6b1c913115b3cad9d88
namln2k <namln2aug2k@gmail.com>
2021-09-09 23:10:45 +0700

    test

--- main.js
+++ main.js
@@ -6,19 +6,19 @@ FB.getLoginStatus(function (response) {
 })
 
 function checkLoginState() {
-        FB.getLoginStatus(function (response) {
-            if (response.status == "connected") {
-                var userID = response.authResponse.userID;
-                //"http://graph.facebook.com/" + userID + "/picture?type=square";
-                console.log(response);
+    FB.getLoginStatus(function (response) {
+        if (response.status == "connected") {
+            var userID = response.authResponse.userID;
+            //"http://graph.facebook.com/" + userID + "/picture?type=square";
+            console.log(response);
 
-                FB.api('/me', { fields: 'id' }, function (response) {
+            FB.api('/me', { fields: 'id' }, function (response) {
+                console.log(response);
+                FB.api('/' + response.id + '/picture?redirect=false', function (response) {
                     console.log(response);
-                    FB.api('/' + response.id + '/picture', function (response) {
-                        console.log(response);
-                    });
                 });
-            }
-            else alert("Đăng nhập thất bại!");
-        });
-    }
+            });
+        }
+        else alert("Đăng nhập thất bại!");
+    });
+}
