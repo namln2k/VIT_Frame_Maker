08c8628db28879cc52570584c19e64af808b6b70
namln2k <namln2aug2k@gmail.com>
2021-09-09 23:54:08 +0700

    test

--- main.js
+++ main.js
@@ -2,20 +2,18 @@ function checkLoginState() {
     FB.getLoginStatus(function (response) {
         if (response.status == "connected") {
             FB.api('/me', { fields: 'id' }, function (response) {
-                console.log(response);
                 FB.api('/' + response.id + '/picture?redirect=false', function (response) {
-                    console.log(response.data);
                     userAvatarBeforeUrl = response.data.url;
                     var canvas = document.createElement("canvas");
                     document.body.appendChild(canvas);
                     var ctx = canvas.getContext("2d");
                     var imageObj1 = new Image(userAvatarBeforeUrl);
                     var imageObj2 = new Image("./frame.png");
+                    console.log("Avatar:\nwidth: " + imageObj1.width + "\nheight: " +imageObj1.height);
+                    console.log("Frame:\nwidth: " + imageObj2.width + "\nheight: " +imageObj2.height);
                     imageObj1.onload = function () {
-                        console.log("Avatar:\nwidth: " + imageObj1.width + "\nheight: " +imageObj1.height);
                         ctx.drawImage(imageObj1, 0, 0, 100, 100);
                         ctx.drawImage(imageObj2, 0, 0, 100, 100);
-                        console.log("Frame:\nwidth: " + imageObj2.width + "\nheight: " +imageObj2.height);
                     };
                     var downloadButton = document.createElement("button");
                     downloadButton.value = "Download";
