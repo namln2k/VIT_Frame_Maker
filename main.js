bc736fc4221d8b7c861ea7adaa16e68892f04230
namln2k <namln2aug2k@gmail.com>
2021-09-09 19:16:17 +0700

    update 09-09-21

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 function createFrame() {
-    var profileLink = document.getElementById("profile-link").value;
-    var subLinkIndex= profileLink.search("facebook.com/");
-    profilePictureApiLink = profileLink.substring(subLinkIndex + 13);
+    var profileLinkInput = document.getElementById("profile-link").value;
+    var subLinkIndex= profileLinkInput.search("facebook.com/");
+    if ((subLinkIndex) == -1) {
+        alert("Liên kết dường như không hợp lệ! Bạn kiểm tra lại nhé!");
+        profileLinkInput.focus();
+    }
+    profilePictureApiLink = "http://graph.facebook.com/" + profileLinkInput.substring(subLinkIndex + 13) + "/picture?type=square";
     console.log(profilePictureApiLink);
 }
