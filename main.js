f6ce6a0b2f6002902ee1839344bab5b3d5069eac
namln2k <namln2aug2k@gmail.com>
2021-09-09 20:05:54 +0700

    update 09-09-21

--- main.js
+++ main.js
@@ -1,16 +1,5 @@
-function createFrame() {
-    // var profileLinkInput = document.getElementById("profile-link").value;
-    // var subLinkIndex= profileLinkInput.search("facebook.com/");
-    // if ((subLinkIndex) == -1) {
-    //     alert("Liên kết dường như không hợp lệ! Bạn kiểm tra lại nhé!");
-    //     profileLinkInput.focus();
-    // }
-    // profilePictureApiLink = "http://graph.facebook.com/" + profileLinkInput.substring(subLinkIndex + 13) + "    picture?type=square";
-    // console.log(profilePictureApiLink);
-    FB.getLoginStatus(function(response) {
-        if (response.status == "connected")
-            alert("Đăng nhập thành công!");
-        else 
-            alert("Đăng nhập thất bại!");
-    });  
+function checkLoginState() {
+    FB.getLoginStatus(function (response) {
+        statusChangeCallback(response);
+    });
 }
