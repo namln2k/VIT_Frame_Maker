dacdd8bd0d3ec07a21ddeca39c235fd392d86431
namln2k <namln2aug2k@gmail.com>
2021-09-09 23:05:33 +0700

    test

--- main.js
+++ main.js
@@ -1,17 +1,24 @@
-function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-        if (response.status == "connected") {
-            var userID = response.authResponse.userID;
-            //"http://graph.facebook.com/" + userID + "/picture?type=square";
+FB.getLoginStatus(function (response) {
+    if (response.status == "connected")
+        FB.logout(function (response) {
             console.log(response);
+        });
+})
 
-            FB.api('/me', { fields: 'id' }, function (response) {
+function checkLoginState() {
+        FB.getLoginStatus(function (response) {
+            if (response.status == "connected") {
+                var userID = response.authResponse.userID;
+                //"http://graph.facebook.com/" + userID + "/picture?type=square";
                 console.log(response);
-                FB.api('/' + response.id + '/picture', function(response) {
+
+                FB.api('/me', { fields: 'id' }, function (response) {
                     console.log(response);
-                  });
-            });
-        }
-        else alert("Đăng nhập thất bại!");
-    });
-}
+                    FB.api('/' + response.id + '/picture', function (response) {
+                        console.log(response);
+                    });
+                });
+            }
+            else alert("Đăng nhập thất bại!");
+        });
+    }
