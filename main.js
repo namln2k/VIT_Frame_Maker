fd7bd0a2776c296a7e5f406f8da8bd5a8c4d9475
namln2k <namln2aug2k@gmail.com>
2021-09-10 00:28:04 +0700

    test

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-var content = document.getElementById("content");
-var c = new Croppie(content, opts);
-c.method(args);
+var basic = $('#content').croppie({
+    viewport: {
+        width: 150,
+        height: 200
+    }
+});
+basic.croppie('bind', {
+    url: 'frame.png',
+    points: [77,469,280,739]
+});
+//on button click
+basic.croppie('result', 'html').then(function(html) {
+    // html is div (overflow hidden)
+    // with img positioned inside.
+});
