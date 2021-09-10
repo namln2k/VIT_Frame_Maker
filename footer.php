0b9542ccab1ecf6cdc33bd61d19f31f640c4f5f0
namln2k <namln2aug2k@gmail.com>
2021-09-10 15:54:57 +0700

    10-09-21

--- footer.php
+++
@@ -1,4 +0,0 @@
-<div id="comments">
-  <p>Created by <a href="http://subinsb.com">Subin Siby</a>.</p>
-  <p>Designs By <a href="https://www.facebook.com/vikaskkunjumon">Vikas</a>, <a href="https://www.facebook.com/maneesh.redline">Maneesh</a></p>
-</div>
